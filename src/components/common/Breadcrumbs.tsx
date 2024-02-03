389d313de2ed80beae758f8a50f0ec1e41a83927
Jeffrey <1115378579@qq.com>
2024-02-03 11:17:47 +0800

    feat: 添加面包屑

---
+++ src/components/common/Breadcrumbs.tsx
@@ -0,0 +1,11 @@
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/react";
+import { NavLink } from "react-router-dom";
+export default function JBreadcrumbs() {
+  return (
+    <Breadcrumbs className="m-2">
+      <BreadcrumbItem>
+        <NavLink to="/">/</NavLink>
+      </BreadcrumbItem>
+    </Breadcrumbs>
+  );
+}
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import { Outlet } from "react-router-dom";
+// import Breadcrumbs from "@/components//common/Breadcrumbs";
 
 function Layout() {
   return (
     <>
       <Header />
+      {/* <Breadcrumbs /> */}
       <div className="container">
         <Outlet />
       </div>
