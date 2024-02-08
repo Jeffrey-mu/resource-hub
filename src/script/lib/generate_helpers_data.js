1d634e43cc78fc6724609e125a130ea662ff446a
Jeffrey <1115378579@qq.com>
2024-02-08 22:41:12 +0800

    wip: 获取awesome文件，解析为json数据，添加备份数据

---
+++ src/script/lib/generate_helpers_data.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+import { join } from "path";
+import { get_absolute_path } from './utils.js';
+export function generate_data() {
+  const { __dirname } = get_absolute_path();
+  let file_list = fs
+    .readdirSync("../helpers")
+    .filter((item) => item.endsWith(".json"));
+  const data = file_list.map((item) => {
+    return {
+      name: item,
+      data: JSON.parse(
+        fs.readFileSync(join(__dirname, "../..//helpers/" + item), "utf8").toString(),
+      ),
+    };
+  });
+  fs.writeFileSync(join(__dirname, "../../../public/helpers.json"), JSON.stringify(data));
+}
